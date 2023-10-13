193a9ca13d4fb7c4644d32706abf10604c251261
Abraham Audu <ab6ril@gmail.com>
2023-10-13 19:17:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Timeseries forecasting of electricity use with AutoEncoder LSTM',
+    author='Author',
+    license='MIT',
+)
